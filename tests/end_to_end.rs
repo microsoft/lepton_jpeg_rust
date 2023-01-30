9c738e2363ec42e91805c323877f2cdb7ef8ee06
Kristof <kristofr@gmail.com>
2023-01-30 11:35:53 +0100

    added tests

--- tests/end_to_end.rs
+++ tests/end_to_end.rs
@@ -52,6 +52,7 @@ fn verify_decode(
         "iphonecrop2",
         "iphoneprogressive",
         "iphoneprogressive2",
+        "progressive_late_dht", // image has huffman tables that come very late which causes a verification failure 
         "narrowrst",
         "nofsync",
         "slrcity",
@@ -98,6 +99,7 @@ fn verify_encode(
             "iphonecrop2",
             "iphoneprogressive",
             "iphoneprogressive2",
+            "progressive_late_dht", // image has huffman tables that come very late which causes a verification failure 
             //"narrowrst",
             //"nofsync",
             "slrcity",
