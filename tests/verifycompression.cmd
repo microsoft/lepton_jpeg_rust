507080db1fffcb0805b1046f086db8823b508388
Kristof <kristofr@gmail.com>
2023-01-31 11:59:54 +0100

    fixed verifycommit

--- tests/verifycompression.cmd
+++ tests/verifycompression.cmd
@@ -1,5 +1,5 @@
 @echo off
-target\release\lepton_rust.exe -verify %1 > error.txt
+..\target\release\lepton_jpeg_util.exe -verify %1 > error.txt
 if errorlevel 1 (
 echo %1 failed %errorlevel%
 echo ------------ >> failedlog.txt
