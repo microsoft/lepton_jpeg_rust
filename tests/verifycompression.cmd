08db124f76f5ceae013618f795066fa6cac2c7b4
Kristof <kristofr@gmail.com>
2023-03-30 07:43:18 +0200

    fix test script

--- tests/verifycompression.cmd
+++ tests/verifycompression.cmd
@@ -1,5 +1,5 @@
 @echo off
-..\target\release\lepton_jpeg_util.exe -verify %1 > error.txt
+..\target\release\lepton_jpeg_util.exe %1 nul > error.txt
 if errorlevel 1 (
 echo %1 failed %errorlevel%
 echo ------------ >> failedlog.txt
