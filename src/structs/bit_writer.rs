6d80cf791c17e44cd65add479805ae9a3bfcd41c
Kristof Roomp <kristofr@gmail.com>
2023-04-03 14:02:38 +0200

    speed up bitwriter by writing 64 bits at a time in the common case (#18)
    * speed up bitwriter by writing 64 bits at a time in the common case
    
    * added randomized unit test for bitwriter

--- src/structs/bit_writer.rs
+++ src/structs/bit_writer.rs
@@ -22,7 +22,8 @@ impl BitWriter {
         };
     }
 
-    fn flush_bytes(&mut self) {
+    #[inline(never)]
+    fn flush_bytes_slowly(&mut self) {
         let mut tmp_current_bit = self.current_bit;
         let mut tmp_fill_register = self.fill_register;
 
@@ -32,20 +33,19 @@ impl BitWriter {
                 self.data_buffer.push(b);
             } else {
                 // escape 0xff here to avoid multiple scans of the same data
-                self.data_buffer.push(0xff);
-                self.data_buffer.push(0);
+                self.data_buffer.extend_from_slice(&[0xff, 0]);
             }
 
             tmp_fill_register <<= 8;
             tmp_current_bit += 8;
         }
 
-        self.current_bit = tmp_current_bit;
         self.fill_register = tmp_fill_register;
+        self.current_bit = tmp_current_bit;
     }
 
     #[inline(always)]
-    pub fn write(&mut self, val: u32, new_bits: u32) {
+    pub fn write(&mut self, mut val: u32, mut new_bits: u32) {
         debug_assert!(
             val < (1 << new_bits),
             "value {0} should fit into the number of {1} bits provided",
@@ -53,19 +53,34 @@ impl BitWriter {
             new_bits
         );
 
-        loop {
-            // first see if everything fits in the current register
-            let tmp_current_bit = self.current_bit;
-            if new_bits <= tmp_current_bit {
-                self.fill_register |= (val as u64).wrapping_shl(tmp_current_bit - new_bits); // support corner case where new_bits is zero, we don't want to panic
-                self.current_bit = tmp_current_bit - new_bits;
-                return;
-            }
+        // first see if everything fits in the current register
+        if new_bits <= self.current_bit {
+            self.fill_register |= (val as u64).wrapping_shl(self.current_bit - new_bits); // support corner case where new_bits is zero, we don't want to panic
+            self.current_bit = self.current_bit - new_bits;
+        } else {
+            // if not, fill up the register so to the 64 bit boundary we can flush it hopefully without any 0xff bytes
+            let fill = self.fill_register | (val as u64).wrapping_shr(new_bits - self.current_bit);
 
-            // if not, flush a byte off the top of the register and try again
-            // there will always be room eventually since we have 64 bits and only allow
-            // 32 bits to be written at a time.
-            self.flush_bytes();
+            new_bits -= self.current_bit;
+            val &= (1 << new_bits) - 1;
+
+            // flush bytes slowly if we have any 0xff bytes or if we are about to overflow the buffer
+            // (overflow check matches implementation in RawVec so that the optimizer can remove the buffer growing code)
+            if (fill & 0x8080808080808080 & !fill.wrapping_add(0x0101010101010101)) != 0
+                || self
+                    .data_buffer
+                    .capacity()
+                    .wrapping_sub(self.data_buffer.len())
+                    < 8
+            {
+                self.fill_register = fill;
+                self.current_bit = 0;
+                self.flush_bytes_slowly();
+            } else {
+                self.data_buffer.extend_from_slice(&fill.to_be_bytes());
+            }
+            self.fill_register = (val as u64).wrapping_shl(64 - new_bits); // support corner case where new_bits is zero, we don't want to panic
+            self.current_bit = 64 - new_bits;
         }
     }
 
@@ -76,7 +91,7 @@ impl BitWriter {
             offset <<= 1;
         }
 
-        self.flush_bytes();
+        self.flush_bytes_slowly();
 
         debug_assert!(
             self.current_bit == 64,
@@ -87,7 +102,7 @@ impl BitWriter {
     // flushes the data buffer while escaping all 0xff characters
     pub fn flush_with_escape<W: Write>(&mut self, w: &mut W) -> anyhow::Result<()> {
         // flush any remaining whole bytes
-        self.flush_bytes();
+        self.flush_bytes_slowly();
 
         w.write_all(&self.data_buffer[..])?;
 
@@ -163,6 +178,56 @@ fn roundtrip_bits() {
 
         for i in 1..2048 {
             assert_eq!(i, r.read(u32_bit_length(i as u32)).unwrap());
+        }
+
+        let mut pad = Some(0xff);
+        r.read_and_verify_fill_bits(&mut pad).unwrap();
+    }
+}
+
+/// verify the the bits roundtrip correctly with random bits
+#[test]
+fn roundtrip_randombits() {
+    use rand::rngs::StdRng;
+    use rand::Rng;
+    use rand::SeedableRng;
+
+    let mut buf = Vec::new();
+
+    const ITERATIONS: usize = 10000;
+
+    let mut rng = StdRng::from_seed([0u8; 32]);
+    let mut test_data = Vec::with_capacity(ITERATIONS);
+
+    for _ in 0..ITERATIONS {
+        let bits = rng.gen_range(0..=16);
+        let v = rng.gen_range(0..=65535) & ((1 << bits) - 1);
+        test_data.push((v as u16, bits as u8));
+    }
+
+    {
+        let mut writer = Cursor::new(&mut buf);
+
+        let mut b = BitWriter::new();
+        for i in &test_data {
+            b.write(i.0 as u32, i.1 as u32);
+
+            // randomly flush the buffer
+            if rng.gen_range(0..50) == 0 {
+                b.flush_with_escape(&mut writer).unwrap();
+            }
+        }
+
+        b.pad(0xff);
+
+        b.flush_with_escape(&mut writer).unwrap();
+    }
+
+    {
+        let mut r = BitReader::new(Cursor::new(&buf));
+
+        for i in &test_data {
+            assert_eq!(i.0, r.read(i.1).unwrap());
         }
 
         let mut pad = Some(0xff);
