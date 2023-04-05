0042483ed2235c142a17a5eae4886cd1e1e391d8
Kristof <kristofr@gmail.com>
2023-04-05 08:28:44 +0200

    add fuzz corpus and minor fixes

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -2,7 +2,7 @@
 
 use std::io::Cursor;
 
-use lepton_jpeg::{decode_lepton, encode_lepton_feat, EnabledFeatures};
+use lepton_jpeg::{decode_lepton, encode_lepton, EnabledFeatures};
 
 use libfuzzer_sys::fuzz_target;
 
@@ -21,7 +21,7 @@ fuzz_target!(|data: &[u8]| {
             max_jpeg_width: 1024,
         };
 
-        r = encode_lepton_feat(&mut Cursor::new(&data), &mut writer, 8, &features);
+        r = encode_lepton(&mut Cursor::new(&data), &mut writer, 8, &features);
     }
 
     let mut original = Vec::new();
