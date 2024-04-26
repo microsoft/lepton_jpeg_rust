11f2e69e30b41302769f3676bf64d8e8e6869277
Kristof <kristofr@gmail.com>
2024-04-26 12:01:53 +0200

    fix const

--- src/consts.rs
+++ src/consts.rs
@@ -4,8 +4,6 @@
  *  This software incorporates material from third parties. See NOTICE.txt for details.
  *--------------------------------------------------------------------------------------------*/
 
-use std::cmp;
-
 #[derive(PartialEq, Debug)]
 pub enum JPegDecodeStatus {
     DecodeInProgress,
