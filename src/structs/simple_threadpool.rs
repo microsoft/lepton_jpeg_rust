3dfacb90f60c6d8961cb7d9bc80f4996d1c6ccf2
mcrumpface <kristofr@gmail.com>
2024-07-27 06:35:40 -0700

    update azure pipeline

--- src/structs/simple_threadpool.rs
+++ src/structs/simple_threadpool.rs
@@ -1,4 +1,18 @@
+/// A simple thread pool implementation that can be used to evaluate closures on separate threads.
+///
+/// The pool will keep a number of threads equal to the number of CPUs available on the system, and
+/// will reuse threads that are idle.
+///
+/// If more tasks are submitted than there are threads, the pool will spawn new threads to handle
+/// the extra tasks.
+///
+/// Why write yet another threadpool? There wasn't one that was that supported dynamically growing
+/// the threadpool (rayon and tokio are all fixed), which is important since otherwise there is
+/// unpredicable latency when the number of tasks submitted is greater than the number of threads.
+///
+/// No unsafe code is used.
 use std::{
+    panic::{catch_unwind, AssertUnwindSafe},
     sync::{
         mpsc::{channel, Sender},
         LazyLock, Mutex,
@@ -6,26 +20,56 @@ use std::{
     thread::{self, spawn},
 };
 
+use anyhow::Result;
+
 static IDLE_THREADS: LazyLock<Mutex<Vec<Sender<Box<dyn FnOnce() + Send + 'static>>>>> =
     LazyLock::new(|| Mutex::new(Vec::new()));
 static NUM_CPUS: LazyLock<usize> = LazyLock::new(|| thread::available_parallelism().unwrap().get());
 
-pub fn evaluate<F, R>(f: F) -> Box<dyn Fn() -> R>
+/// Evaluates a closure on a thread from the thread pool. Returns a closure that will block until the
+/// result is available, and return that result.
+pub fn evaluate<F, R>(f: F) -> Box<dyn Fn() -> Result<R>>
 where
     R: Send + 'static,
-    F: FnOnce() -> R + Send + 'static,
+    F: FnOnce() -> Result<R> + Send + 'static,
 {
+    // channel used to send result back to executor
     let (tx, rx) = channel();
 
     execute(move || {
-        let r = f();
-        tx.send(r).unwrap();
+        let r = catch_unwind(AssertUnwindSafe(f));
+
+        let r = match r {
+            Ok(r) => r,
+            Err(panic_info) => {
+                let error_message = if let Some(s) = panic_info.downcast_ref::<&str>() {
+                    format!("Panic occurred: {}", s)
+                } else if let Some(s) = panic_info.downcast_ref::<String>() {
+                    format!("Panic occurred: {}", s)
+                } else {
+                    "Panic occurred: Unknown panic payload".to_string()
+                };
+
+                Err(anyhow::Error::msg(error_message))
+            }
+        };
+        let _ = tx.send(r);
     });
 
-    Box::new(move || rx.recv().unwrap())
+    // return a closure that will block until the result is available
+    Box::new(move || match rx.recv() {
+        Ok(r) => r,
+        Err(e) => Err(anyhow::Error::new(e)),
+    })
 }
 
-pub fn execute<F>(f: F)
+#[allow(dead_code)]
+pub fn get_idle_threads() -> usize {
+    IDLE_THREADS.lock().unwrap().len()
+}
+
+/// Executes a closure on a thread from the thread pool. Does not block or return any result.
+fn execute<F>(f: F)
 where
     F: FnOnce() + Send + 'static,
 {
@@ -39,7 +83,10 @@ where
 
             loop {
                 if let Ok(mut i) = IDLE_THREADS.lock() {
+                    // stick back into list of idle threads if there aren't more than
+                    // the number of cpus already there.
                     if i.len() > *NUM_CPUS {
+                        // just exits the thread
                         break;
                     }
                     i.push(tx.clone());
@@ -60,11 +107,17 @@ where
 #[test]
 fn test_threadpool() {
     let mut results = Vec::new();
-    for i in 0..100 {
-        results.push(evaluate(move || i));
+    for i in 0usize..100 {
+        results.push(evaluate(move || Ok(i)));
     }
 
     for i in 0..100 {
-        assert_eq!(results[i](), i);
+        let r = results[i]();
+        match r {
+            Ok(r) => assert_eq!(r, i),
+            Err(_) => panic!(),
+        }
     }
+
+    println!("Idle threads: {}", get_idle_threads());
 }
