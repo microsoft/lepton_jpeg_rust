5b6949c108514e92a730fa8365e32a6627caab23
Kristof <kristofr@gmail.com>
2024-12-03 10:54:10 +0100

    module docs

--- src/jpeg/mod.rs
+++ src/jpeg/mod.rs
@@ -1,7 +1,12 @@
-/// Module for reading and recreation of JPEGs without the loss of any information.
-///
-/// This means that it should be possible to reconstruct bit-by-bit an exactly identical
-/// JPEG using this code.
+//! Module for reading and recreation of JPEGs without the loss of any information.
+//!
+//! This means that it should be possible to reconstruct bit-by-bit an exactly identical
+//! JPEG file from the input.
+//!
+//! Note that we never actually decode the JPEG into pixels, since the DCT is lossy, so
+//! processing needs to be done at the DCT coefficient level and keep the coefficients in
+//! the BlockBasedImage identical.
+
 mod bit_reader;
 mod bit_writer;
 mod component_info;
