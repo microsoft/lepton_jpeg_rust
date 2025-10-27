7461e590f6c8cf4611c44d149119c0eedfc65e70
Kristof Roomp <kristofr@gmail.com>
2025-10-27 16:42:14 +0100

    Add publish for python to pypi (#162)

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,16 +1,108 @@
+use lepton_jpeg::{LeptonThreadPool, SingleThreadPool};
 use pyo3::prelude::*;
-use pyo3::types::PyBytes;
+use pyo3::types::{PyBytes, PyDict};
 use std::io::Cursor;
+use std::sync::LazyLock;
+
+enum ThreadOptions {
+    SingleThread,
+    PerCpu,
+    NoLimit,
+}
+
+struct RayonThreadPool {
+    pool: LazyLock<rayon::ThreadPool>,
+}
+
+impl LeptonThreadPool for RayonThreadPool {
+    fn run(&self, f: Box<dyn FnOnce() + Send + 'static>) {
+        self.pool.spawn(f);
+    }
+}
+
+static RAYON_THREAD_POOL: RayonThreadPool = RayonThreadPool {
+    pool: LazyLock::new(|| rayon::ThreadPoolBuilder::new().build().unwrap()),
+};
+
+fn parse_config(
+    config: Option<&Bound<'_, PyDict>>,
+) -> PyResult<(lepton_jpeg::EnabledFeatures, ThreadOptions)> {
+    let mut features = lepton_jpeg::EnabledFeatures::compat_lepton_vector_write();
+
+    let mut threads = ThreadOptions::PerCpu;
+
+    if let Some(cfg) = config {
+        for (key, value) in cfg.iter() {
+            let key_str: &str = key.extract()?;
+            match key_str {
+                "max_jpeg_width" => {
+                    let val: u32 = value.extract()?;
+                    features.max_jpeg_width = val;
+                }
+                "max_jpeg_height" => {
+                    let val: u32 = value.extract()?;
+                    features.max_jpeg_height = val;
+                }
+                "progressive" => {
+                    let val: bool = value.extract()?;
+                    features.progressive = val;
+                }
+                "reject_dqts_with_zeros" => {
+                    let val: bool = value.extract()?;
+                    features.reject_dqts_with_zeros = val;
+                }
+                "max_partitions" => {
+                    let val: u32 = value.extract()?;
+                    features.max_threads = val;
+                }
+                "max_jpeg_file_size" => {
+                    let val: u32 = value.extract()?;
+                    features.max_jpeg_file_size = val;
+                }
+                "threads" => match value.extract::<&str>()? {
+                    "single" => threads = ThreadOptions::SingleThread,
+                    "per_cpu" => threads = ThreadOptions::PerCpu,
+                    "no_limit" => threads = ThreadOptions::NoLimit,
+                    _ => {
+                        return Err(pyo3::exceptions::PyValueError::new_err(format!(
+                            "Invalid threads option: {}",
+                            value.extract::<&str>()?
+                        )));
+                    }
+                },
+                _ => {
+                    return Err(pyo3::exceptions::PyValueError::new_err(format!(
+                        "Unknown configuration key: {}",
+                        key_str
+                    )));
+                }
+            }
+        }
+    }
+    Ok((features, threads))
+}
 
 #[pyfunction]
-pub fn compress_bytes(py: Python, data: &[u8]) -> PyResult<Py<PyAny>> {
+#[pyo3(signature = (data, config=None))]
+pub fn compress_bytes(
+    py: Python,
+    data: &[u8],
+    config: Option<&Bound<'_, PyDict>>,
+) -> PyResult<Py<PyAny>> {
     let mut compressed = Vec::new();
+
+    let (features, threads) = parse_config(config)?;
+    let single = SingleThreadPool::default();
 
     lepton_jpeg::encode_lepton(
         &mut Cursor::new(data),
         &mut Cursor::new(&mut compressed),
-        &lepton_jpeg::EnabledFeatures::compat_lepton_vector_write(),
-        &lepton_jpeg::DEFAULT_THREAD_POOL,
+        &features,
+        match threads {
+            ThreadOptions::SingleThread => &single,
+            ThreadOptions::PerCpu => &RAYON_THREAD_POOL,
+            ThreadOptions::NoLimit => &lepton_jpeg::DEFAULT_THREAD_POOL,
+        },
     )
     .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(format!("Compression failed: {}", e)))?;
 
@@ -18,14 +110,26 @@ pub fn compress_bytes(py: Python, data: &[u8]) -> PyResult<Py<PyAny>> {
 }
 
 #[pyfunction]
-pub fn decompress_bytes(py: Python, data: &[u8]) -> PyResult<Py<PyAny>> {
+#[pyo3(signature = (data, config=None))]
+pub fn decompress_bytes(
+    py: Python,
+    data: &[u8],
+    config: Option<&Bound<'_, PyDict>>,
+) -> PyResult<Py<PyAny>> {
     let mut decompressed = Vec::new();
+
+    let (features, threads) = parse_config(config)?;
+    let single = SingleThreadPool::default();
 
     lepton_jpeg::decode_lepton(
         &mut Cursor::new(data),
         &mut Cursor::new(&mut decompressed),
-        &lepton_jpeg::EnabledFeatures::compat_lepton_vector_write(),
-        &lepton_jpeg::DEFAULT_THREAD_POOL,
+        &features,
+        match threads {
+            ThreadOptions::SingleThread => &single,
+            ThreadOptions::PerCpu => &RAYON_THREAD_POOL,
+            ThreadOptions::NoLimit => &lepton_jpeg::DEFAULT_THREAD_POOL,
+        },
     )
     .map_err(|e| {
         pyo3::exceptions::PyRuntimeError::new_err(format!("Decompression failed: {}", e))
--- python/tests/test_compress.py
+++ python/tests/test_compress.py
@@ -5,8 +5,16 @@ def test_compress_decompress():
     with open("../images/slrcity.jpg", "rb") as f:
         jpg_data = f.read()
 
-    compressed = lepton_jpeg_python.compress_bytes(jpg_data)
-    decompressed = lepton_jpeg_python.decompress_bytes(compressed)
+    config = {
+        "max_jpeg_width": 8196,
+        "max_jpeg_height": 8196,
+        "progressive": False,
+        "reject_dqts_with_zeros": True,
+        "max_partitions": 8,
+        "max_jpeg_file_size": 128 * 1024 * 1024 }
+
+    compressed = lepton_jpeg_python.compress_bytes(jpg_data, config)
+    decompressed = lepton_jpeg_python.decompress_bytes(compressed, config)
 
     assert jpg_data == decompressed
     print("Compression and decompression successful!")
